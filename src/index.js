38e14e84d4446f153d3c9c5fad769a45071b1e23
Vladyslav Tyshchenko <vvant97@gmail.com>
2022-06-23 11:23:32 +0300

    refactor: minificate function and remove useless features

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 module.exports = function toReadable (number) {
   
-  const units = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-  const dozen = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 
-                 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  const units = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+  const dozen = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 
+                 'seventeen', 'eighteen', 'nineteen'];
   const dozens = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
   const hundreds = ['one hundred', 'two hundred', 'three hundred', 'four hundred', 'five hundred', 
                     'six hundred', 'seven hundred', 'eight hundred', 'nine hundred'];
@@ -15,26 +15,22 @@ module.exports = function toReadable (number) {
 
   let result = '';
 
-  let i = 0;
-
-  while (i < valueStringLength) {
-
-    if (number < 10) {
-      return `${units[valueFirstIndex]}`;
-    } else if (number > 9 && number < 20) {
-      return `${dozen[valueLastIndex]}`;
-    } else if (number > 19 && number < 100) {
-      return `${dozens[valueFirstIndex - 2]} ${units[valueLastIndex].replace('zero', '')}`.trim();
-    } else if (number > 99 && number < 1000 && valueSecondIndex < 1 && valueLastIndex >= 0) {
-      return `${hundreds[valueFirstIndex - 1]} ${units[valueLastIndex].replace('zero', '')}`.trim();
-    } else if (number > 99 && number < 1000 && valueSecondIndex >= 1 && valueSecondIndex < 2) {
-      return `${hundreds[valueFirstIndex - 1]} ${dozen[valueLastIndex]}`;
-    } else if (number > 99 && number < 1000 && valueSecondIndex >= 2) {
-      return `${hundreds[valueFirstIndex - 1]} ${dozens[valueSecondIndex - 2]} ${units[valueLastIndex].replace('zero', '')}`.trim();
-    } else {
-      return result;
-    }
-
+  if (number > 0 && number < 10) {
+    result = `${units[valueFirstIndex]}`;
+  } else if (number > 9 && number < 20) {
+    result = `${dozen[valueLastIndex]}`;
+  } else if (number > 19 && number < 100) {
+    result = `${dozens[valueFirstIndex - 2]} ${units[valueLastIndex]}`;
+  } else if (valueSecondIndex < 1 && valueLastIndex >= 0) {
+    result = `${hundreds[valueFirstIndex - 1]} ${units[valueLastIndex]}`;
+  } else if (valueSecondIndex >= 1 && valueSecondIndex < 2) {
+    result = `${hundreds[valueFirstIndex - 1]} ${dozen[valueLastIndex]}`;
+  } else if (valueSecondIndex >= 2) {
+    result = `${hundreds[valueFirstIndex - 1]} ${dozens[valueSecondIndex - 2]} ${units[valueLastIndex]}`;
+  } else {
+    result = number === 0 ? 'zero' : false;
   }
+
+  return result.trim();
 
 };
